5e2a8d4587ba48cefaca658a4523d3ab919da281
denisgorlachev <117777771+denisgorlachev@users.noreply.github.com>
2022-11-09 19:37:31 +0200

    Добавьте файлы проекта.

---
+++ лаба 2.1/Program.cs
@@ -0,0 +1,13 @@
+﻿//Горлачев средний уровень 
+Console.WriteLine("Введите число k: ");
+double k = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число l: ");
+double l = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+double n = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число m: ");
+double m = double.Parse(Console.ReadLine());
+if ((k + l + m + n > 0 && k > 0 && 2 * m > l) || (k + l + m + n > 0 && k < 0 && n > 3 * l))
+    Console.WriteLine("Условия выполняется");
+else
+    Console.WriteLine("Условия не выполянется");
